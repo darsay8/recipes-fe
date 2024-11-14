4c8faf6f0c97e22f3009550328e8656e25410e7c
darsay8 <darsay.one@gmail.com>
2024-11-13 21:18:04 -0300

    Refactor security configuration and update login form to use email instead of username

--- src/main/java/dev/rm/recipes/config/SecurityConfig.java
+++ src/main/java/dev/rm/recipes/config/SecurityConfig.java
@@ -1,39 +1,15 @@
 package dev.rm.recipes.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  // @Bean
-  // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-  // Exception {
-  // http
-  // .csrf(csrf -> csrf.disable())
-  // .sessionManagement(session ->
-  // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-  // .authorizeHttpRequests(auth -> auth
-  // .requestMatchers("/login", "/recipes", "/").permitAll()
-  // .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-  // .requestMatchers("/recipes/**").authenticated()
-  // .anyRequest().authenticated())
-  // .addFilterBefore(jwtAuthenticationFilter(),
-  // UsernamePasswordAuthenticationFilter.class);
-
-  // return http.build();
-  // }
-
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -42,18 +18,5 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
     return http.build();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-    configuration.setExposedHeaders(Arrays.asList("Authorization"));
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 }
